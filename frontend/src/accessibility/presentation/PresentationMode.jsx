fd25ef85e2190e316a508dd2fb07f2cc50de6fe5
Nibret-21 <nibretadamu654@gmail.com>
2026-07-07 06:55:06 +0300

    fix db and safeExecute exports

--- frontend/src/accessibility/presentation/PresentationMode.jsx
+++ frontend/src/accessibility/presentation/PresentationMode.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import {presentationSlides} from "./presentationSlides";
-import usePresentationKeyboard from "./presentationKeyboard";
+import usePresentationkeyboard from "./presentationkeyboard";
 import {
   readSlide,
   repeatSlide,
